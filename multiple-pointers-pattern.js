830718ba2abd16274d30eadb54e5eb8cc42814c6
David L <dvdlq99@gmail.com>
2021-06-17 20:46:18 +0200

    added multiple-pointers-pattern exercise

---
+++ multiple-pointers-pattern.js
@@ -0,0 +1,18 @@
+function countUniqueValues(arr){
+  
+    if(arr.length === 0){
+        return 0;
+    }
+    
+    let refPoint = 0;
+    let refValue = arr[refPoint];
+    
+    for(let i = 1; i < arr.length; i++){
+        if(refValue !== arr[i]){
+            refPoint++;
+            refValue = arr[i];
+        }
+    }
+    
+    return refPoint + 1;;
+}
