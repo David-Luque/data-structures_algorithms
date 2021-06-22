5946ca378952d3b67792bfd282277f3f639e4c0c
David L <dvdlq99@gmail.com>
2021-06-22 18:35:46 +0200

    binary search practice

---
+++ binary-Search.js
@@ -0,0 +1,13 @@
+function binarySearch(arr, elem) {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((end + start) / 2);
+
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]) end = middle - 1;
+        else start = middle + 1;
+
+        middle = Math.floor((end + start) / 2);
+    }
+    return arr[middle] === elem ? middle : -1;
+}
